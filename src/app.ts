f5681b817340bd6eb62b119a45f8fc9f0898bb8e
ShebangDog <twilight.sky.asterisk@gmail.com>
2025-07-06 13:14:52 +0900

    ReactionRanking機能をappに実装する

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,45 @@
-import { App } from '@slack/bolt';
-import * as dotenv from 'dotenv';
+import { App } from "@slack/bolt";
+import { readEnvironment } from "./environment/AppEnvironment";
+import { getAllChannelReactionCounts } from "./repository/ReactionRepository";
+import { createMessageImpl, doTask as doReactionRankingMainTask } from "./services/ReactionRankingService";
 
-// .envファイルから環境変数を読み込む
-dotenv.config();
+const {
+  SLACK_BOT_TOKEN,
+  SLACK_APP_TOKEN, // Socket Modeに必要
+  SLACK_REACTION_RANKING_CHANNEL_ID: ReactionRankingChannelId,
+  SIGNING_SECRET,
+} = readEnvironment();
 
-// アプリを初期化
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN, // Socket Modeに必要
-  socketMode: true, // Socket Modeを有効にする
-});
+  token: SLACK_BOT_TOKEN,
+  appToken: SLACK_APP_TOKEN, // Socket Modeに必要
+  socketMode: false,
+  signingSecret: SIGNING_SECRET,
+})
 
-// Botへのメンションをリッスン
-app.event('app_mention', async ({ event, say }) => {
-  try {
-    // メンションしてきたユーザーに返信
-    await say(`<@${event.user}> こんにちは！`);
-  } catch (error) {
-    console.error(error);
-  }
-});
+async function publishMessage(app: App, id: string, text: string) {
+  return app.client.chat.postMessage({
+    channel: id,
+    text: text
+  });
+}
 
-// アプリを起動
-(async () => {
-  await app.start();
-  console.log('⚡️ Bolt app is running!');
-})();
+const printMessageImpl = async (text: string): Promise<void> => {
+  console.log(text);
+
+  await publishMessage(app, ReactionRankingChannelId, text)
+    .catch(error => console.error(error));
+}
+
+const fetchReactionImpl = () => getAllChannelReactionCounts(app);
+
+// Run the application
+async function main(): Promise<void> {
+  await doReactionRankingMainTask(
+    createMessageImpl,
+    printMessageImpl,
+    fetchReactionImpl,
+  )
+}
+// Execute main function
+main().catch(console.error);
