92d49767919d597174b2dfebf799f73a302a92e8
ShebangDog <twilight.sky.asterisk@gmail.com>
2025-07-06 13:14:28 +0900

    ReactionRanking機能を実装する

---
+++ src/services/ReactionRankingService.ts
@@ -0,0 +1,59 @@
+import { compareByCount, Reaction, toReactionFormat } from "../models/reaction"
+
+const RankInLimit = 5
+
+const doTask = async (
+    createMessage: (reactionRanking: (Reaction | null)[]) => string,
+    printMessage: (text: string) => Promise<void>,
+    fetchReaction: () => Promise<Reaction[]>
+): Promise<void> => {
+    const reactionList = await fetchReaction()
+    
+    const rankingReaction: (Reaction | null)[] = calculateReactionRanking(reactionList, RankInLimit)
+
+    const maybeMessage = createMessage(rankingReaction)
+
+    printMessage(maybeMessage)
+}
+
+const First = 0
+const Offset = 1
+/*
+ * @description
+ * rankInLimitとしてランクイン数を指定し、ランクインしたリアクションをランキング順に返却する
+ * ランクインしていない順位はnullで埋める
+ */
+const calculateReactionRanking = (reactionList: Reaction[], rankInLimit: number): (Reaction | null)[] => {
+    const sortedByCount = [...reactionList].sort(compareByCount)
+
+    const result = sortedByCount.slice(First, rankInLimit)
+    
+    return [...result, ...Array(rankInLimit - result.length).fill(null)]
+}
+
+const createRankInTemplate = (reaction: Reaction, rankAsIndex: number): string => {
+    const rank = rankAsIndex + Offset;
+    const formatted = toReactionFormat(reaction);
+
+    return `${rank}位: ${formatted} (${reaction.count}回)`;
+  }
+
+  const createNotRankInTemplate = (_reaction: null, rankAsIndex: number): string => {
+    const rank = rankAsIndex + Offset;
+
+    return `${rank}位: ランクインなし`;
+  }
+
+const createMessageImpl = (reactionRanking: (Reaction | null)[]): string => {
+    const message = reactionRanking.map(
+        (_, index) => _ === null 
+        ? createNotRankInTemplate(_, index) 
+        : createRankInTemplate(_, index)
+    ).join("\n");
+
+    return message
+}
+export {
+    createMessageImpl, doTask
+}
+
